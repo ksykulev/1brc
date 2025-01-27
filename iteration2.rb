ddb157c57d960cd93dc7be95ba895b04d0185256
Konstantin Sykulev <konst@sykulev.com>
2025-01-27 11:56:12 -0600

    iteration 2 stats

--- iteration2.rb
+++ iteration2.rb
@@ -1,6 +1,6 @@
 require 'ruby-prof'
 
-#result = RubyProf::Profile.profile do
+result = RubyProf::Profile.profile do
   stations = {}
   file = File.foreach('measurements.txt') do |line|
   station, temp = line.split(';')
@@ -27,11 +27,11 @@ require 'ruby-prof'
       printed = true
     end
   end 
-#end
+end
 
-# File.open "iteration2-profile-stack.html", 'w+' do |file|
-#   RubyProf::CallStackPrinter.new(result).print(file)
-# end
+File.open "iteration2-profile-stack.html", 'w+' do |file|
+  RubyProf::CallStackPrinter.new(result).print(file)
+end
 
 # $ time ruby iteration2.rb 
 # Banjul=-23.3/25.99548007567204/82.1
